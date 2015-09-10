1c0953755b49848fd72c8b37d67304227081db69
0nin <foursilico@gmail.com>
2015-09-10 18:32:59 +0300

    init.el updtated

--- init.el
+++ init.el
@@ -339,7 +339,7 @@
 (custom-imenu)
 (custom-other)
 (custom-org)
-(elscreen-start)
+;; (elscreen-start)
 (require 'multiple-cursors)
 (global-set-key (kbd "C-S-c C-S-c") 'mc/edit-lines)
 (global-set-key (kbd "C->") 'mc/mark-next-like-this)
